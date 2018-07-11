3d8033e310abae8adfcec44e7577e56297255f61
Ian Smith <ian.smith@pnnl.gov>
2018-07-11 13:57:59 -0700

    pulling back debian as well because no binaries for debain 9

--- spec/support/supported_platforms.rb
+++ spec/support/supported_platforms.rb
@@ -1,6 +1,6 @@
 def supported_platforms
   _platforms = {
-    'debian' => ['9.3'],
+    'debian' => ['8.9', '9.3'],
     'ubuntu' => ['14.04', '16.04'],
     'redhat' => ['6.9', '7.4'],
     'centos' => ['6.9', '7.4.1708'],
