0479767975d7e7ae5914a9c0e3583c3c695b489d
Ian Smith <ian.smith@pnnl.gov>
2018-07-21 21:23:01 -0700

    updating postgresql version, adding poise git and python

--- metadata.rb
+++ metadata.rb
@@ -8,12 +8,15 @@ issues_url        'https://github.com/sitle/chef-bareos/issues'
 source_url        'https://github.com/sitle/chef-bareos.git'
 version           '4.0.0'
 
-chef_version      '>= 12.19'
+chef_version      '>= 13.4.19'
 
 supports          'debian', '>= 8'
 supports          'ubuntu', '>= 14' # No binaries for bareos 15 on ubuntu 16
 supports          'redhat', '>= 6'
 supports          'centos', '>= 6'
 
+depends           'chef-sugar'
 depends           'openssl', '>= 4.0'
-depends           'postgresql', '< 7.0'
+depends           'postgresql', '~> 7.0'
+depends           'poise-git'
+depends           'poise-python'
